a6257fa105281cf635b60ab14ef1dbb5217741f7
sudokoko <philljad000@hsestudents.org>
2022-12-16 01:35:55 -0500

    fuck

--- index.js
+++ index.js
@@ -11,27 +11,21 @@ app.use('/', directory(__dirname + '/content'));
 app.use('/shit', express.static('static'))
 
 /* create the upload endpoint */
-
 app.use(fileUpload());
 
 app.post('/upload', function(req, res) {
-  let sampleFile;
+  let uploadThisFuckingFile;
   let uploadPath;
 
-  if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).send('No files were uploaded.');
-  }
-
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  sampleFile = req.files.sampleFile;
-  uploadPath = __dirname + '/content/' + sampleFile.name;
+  uploadThisFuckingFile = req.files.uploadThisFuckingFile;
+  uploadPath = __dirname + '/content/' + uploadThisFuckingFile.name;
 
   // Use the mv() method to place the file somewhere on your server
-  sampleFile.mv(uploadPath, function(err) {
+  uploadThisFuckingFile.mv(uploadPath, function(err) {
     if (err)
       return res.status(500).send(err);
-
-    res.status(200).send('Upload completed');
+    res.status(200).end()
   });
 });
 
