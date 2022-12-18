56c5345c2d38cee10c4c181610146361bf48dff2
sudokoko <philljad000@hsestudents.org>
2022-12-18 15:44:15 -0500

    hotfix: block index.html

--- index.js
+++ index.js
@@ -14,24 +14,16 @@ app.post('/api/v1/upload', function(req, res) {
   uploadThisFuckingFile = req.files.uploadThisFuckingFile;
   uploadPath = '/var/www/html/' + uploadThisFuckingFile.name;
 
-  uploadThisFuckingFile.mv(uploadPath, function(err) {
-    if (err)
-      return res.status(500).send(err);
-    res.status(200).send("File was uploaded successfully, it should now appear on the dump.").end();
-  });
+  if(uploadPath === "/var/www/html/index.html") {
+    return res.status(403).send("Uploading an index.html file is forbidden!").end();
+  } else {
+    uploadThisFuckingFile.mv(uploadPath, function(err) {
+      if (err)
+        return res.status(500).send(err);
+      res.status(200).send("File was uploaded successfully, it should now appear on the dump.").end();
+    });
+  }
 });
-
-/*  -- broken --
-  app.get('/api/v1/delete', function(req, res) {
-    if(req.query.token = process.env.MODERATION_TOKEN)
-    {
-      fs.unlinkSync("/var/www/html/" + req.query.path);
-      res.status(200).send("File has been deleted.").end();
-    } else {
-      res.status(403).send("You are not authorized to perform this action").end();
-    }
-  });
-*/
 
 /* serve the uploader */
 app.use('*', express.static('static'));
