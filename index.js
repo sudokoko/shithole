847e7d59e4996e6a5537f9513a1660a55e81b18c
sudokoko <philljad000@hsestudents.org>
2022-12-18 11:01:32 -0500

    silly little changes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const directory = require('serve-index');
-const fileUpload = require('express-fileupload')
-const app = express()
-const port = 8000
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const fs = require('fs');
+const app = express();
+const port = 8000;
 
 /* create the upload endpoint */
 app.use(fileUpload());
@@ -17,19 +17,31 @@ app.post('/api/v1/upload', function(req, res) {
   uploadThisFuckingFile.mv(uploadPath, function(err) {
     if (err)
       return res.status(500).send(err);
-    res.status(200).send("File was uploaded successfully, it should now appear on the dump.").end()
+    res.status(200).send("File was uploaded successfully, it should now appear on the dump.").end();
   });
 });
 
+app.delete('/api/v1/delete', function(req, res) {
+  let auth = req.params.token;
+  let file = req.params.path; 
+
+  if(auth = process.env.MODERATION_TOKEN)
+  {
+    fs.unlinkSync("/var/www/html/" + file);
+    res.status(200).send("File has been deleted.").end();
+  } else {
+    res.status(403).send("You are not authorized to perform this action").end();
+  }
+});
 
 /* serve the uploader */
-app.use('*', express.static('static'))
+app.use('*', express.static('static'));
 
 
 app.listen(port, () => {
     if(port != 80 || port != 443)
     {
-        console.warn(`You will probably need to do some jank shit with a tool like Portzilla to get the front-end accessible!`)
+        console.warn(`You will probably need to do some jank shit with a tool like a port proxy to get the front-end accessible!`);
     }
-  console.log(`App listening on port ${port}`)
+  console.log(`App listening on port ${port}`);
 })
