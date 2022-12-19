988a1a60af24faf3199a00797da1a0fd7ae25be6
sudokoko <philljad000@hsestudents.org>
2022-12-19 15:04:34 -0500

    piss fart

--- index.js
+++ index.js
@@ -16,8 +16,12 @@ app.post('/api/v1/upload', function(req, res) {
   uploadPath = '/var/www/html/' + uploadThisFuckingFile.name;
   literalFilePath = uploadThisFuckingFile.name;
 
-  if(uploadPath.includes("index")) {
-    var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
+  const banList = fs.readFileSync("/var/www/html/configuration/bans.txt");
+  var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
+
+  if(banList.includes(ip)) {
+    res.status(403).send("You are permanently banned from accessing this shithole instance for uploading illegal, malicious, or copyrighted content.").end();
+  } else if(uploadPath.includes("index")) {
     return res.status(403).send("您好 "+ ip + "，您似乎正在尝试上传 index.html 文件。 不要那样做你这只傻鹅！").end();
   } else if(literalFilePath.includes("/") || literalFilePath.includes("\\") || literalFilePath.includes("..") || literalFilePath.includes("%2F") || literalFilePath.includes("%5C")) {
     res.status(403).send("no! th-thats dirty... you can't do that..!").end()
@@ -27,6 +31,16 @@ app.post('/api/v1/upload', function(req, res) {
         return res.status(500).send(err);
       res.status(200).send("File was uploaded successfully, it should now appear on the dump.").end();
     });
+  }
+});
+
+/* bans */
+app.get('*', function(req, res) {
+  const banList = fs.readFileSync("/var/www/html/configuration/bans.txt");
+  var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
+
+  if(banList.includes(ip)) {
+    res.status(403).send("You are permanently banned from accessing this shithole instance for uploading illegal, malicious, or copyrighted content.").end();
   }
 });
 
